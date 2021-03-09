049df9cc4cf153764633415da2558f9e49fd26ae
1-mr-robot <dhyan.bohra@gmail.com>
2021-03-09 17:45:19 +0800

    Position control w/ and w/o Velocity

--- epos_arm_control/src/imu_position_control.cpp
+++ epos_arm_control/src/imu_position_control.cpp
@@ -7,6 +7,7 @@
 epos_arm_control::epos arm_control;
 std_msgs::Float64MultiArray angle;
 std_msgs::Float64 error;
+std_msgs::Float64 error_prev;
 
 
 
@@ -65,6 +66,8 @@ int main(int argc, char **argv)
 	int lResult = MMC_FAILED;
 	unsigned int ulErrorCode = 0;
 	long error_inc;
+	float proportional;
+	float derivative;
 
     // Print maxon headerline
     PrintHeader();
@@ -101,7 +104,7 @@ int main(int argc, char **argv)
 
 
 	ros::Publisher pub=n.advertise<std_msgs::Float64>("pid_position",1000);
-	ros::Rate loop_rate(100);
+	ros::Rate loop_rate(200);
 	ros::Subscriber sub1 = n.subscribe("flexion_angle", 1000, arrayCallback);
 	ros::Subscriber sub = n.subscribe("exoskel_control", 1000, moveArm);
 	
@@ -115,21 +118,39 @@ int main(int argc, char **argv)
 			int absolute=1;
 			int relative=0;
 			float Kp=1;
+			float Kd=0.0001;
+			float desired_angle=0;
+			long velocity= 10;
+			int current_velocity;
 
 			msg << "move to position = " << (long)arm_control.angle << ", node = " << g_usNodeId<<"\n";
 			LogInfo(msg.str());
 
-			error.data=-(arm_control.angle-angle.data[2]);
-			error_inc=Kp*error.data*16384*81/360;
+			// error.data=(arm_control.angle-angle.data[2]);
+			// error_inc=Kp*error.data*16384*81/360;
+			error.data=(arm_control.angle-angle.data[2]);
+			error_inc=error.data*16384*81/360;
+
+			proportional=Kp*error_inc;
+			derivative=Kd*((error_inc-error_prev.data)/0.005);
+			error_prev.data=error_inc;
+
+			//desired_angle=proportional;
+			desired_angle=proportional+derivative;
+
     		printf("error= %lf \n",error.data);
 
-			if(angle.data[2]>arm_control.angle-0.5 && angle.data[2]<arm_control.angle+0.5)
+			if(angle.data[2]>arm_control.angle-0.2 && angle.data[2]<arm_control.angle+0.2)
     		{
+				HaltVelocity(g_pKeyHandle,g_usNodeId);
 				HaltPosition(g_pKeyHandle,g_usNodeId);
+				
     		}
 			else
 			{
-				MoveToPosition(g_pKeyHandle, g_usNodeId, error_inc,relative, &ulErrorCode);
+				MoveWithVelocity(g_pKeyHandle, g_usNodeId,velocity,&ulErrorCode);
+				get_velocity(g_pKeyHandle,g_usNodeId,&current_velocity,&ulErrorCode);
+				MoveToPosition(g_pKeyHandle, g_usNodeId, desired_angle,relative, &ulErrorCode);
 			}
 		}
 		// else
